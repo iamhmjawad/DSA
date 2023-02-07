ef679cfc3af204f61f6b803980c2257e439b7681
iamhmjawad <thisishmjawad@gmail.com>
2023-02-07 12:19:55 +0500

    Feb 07/23

--- MAth/BintoDec.js
+++ MAth/BintoDec.js
@@ -2,6 +2,7 @@ function binToDec(binary) {
     let decimal = 0
     for (let i = 0; i < binary.length; i++) {
         decimal += binary[binary.length - 1 - i] * (2 ** i)
+        // 2^5 + 2^4 + 2^3 + 2^2 + 2^1 + 2^0
     }
     return decimal
 }
